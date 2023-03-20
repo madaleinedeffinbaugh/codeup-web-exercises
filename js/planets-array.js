2f30c7751b408d353e86607222a2d2b36075359d
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-03-19 19:33:18 -0600

    First and second TODO completed

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,9 +19,11 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift("The Sun");
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
